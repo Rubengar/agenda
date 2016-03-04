2f6bd4165e0a89847bde41158de8257c1919e31d
unknown <you@example.com>
2016-03-04 13:06:21 +0100

    Añadido el metodo printAllNames

--- AgendaTelefonica.java
+++ AgendaTelefonica.java
@@ -37,4 +37,11 @@ public class AgendaTelefonica
         return agenda.get(name);
     }
 
+    /**
+     * Print all keys
+     */
+    public void printAllNames()
+    {
+        System.out.println(agenda.keySet());
+    }
 }
